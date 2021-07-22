b02a72fbfcf684997574b8f39433a2910effa991
smit-00 <copypaste121999@gmail.com>
2021-07-22 22:11:43 +0530

    cccc

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          "This one line here is the sole of this app huh ;-) m n m x"
-          <br/>
-        </p>
-          <h5>Dockerfile ka working directory WORKDIR jo hoga wo docker-compose.yml me use hoga volumes mention karne ke lie</h5>
+        <p>"This one line here is the sole of this app huh ;-) m n m x"</p>
+        <p>"Line 2 xoxo</p>
+        <br />
+        <h5>
+          Dockerfile ka working directory WORKDIR jo hoga wo docker-compose.yml
+          me use hoga volumes mention karne ke lie
+        </h5>
       </header>
     </div>
   );
